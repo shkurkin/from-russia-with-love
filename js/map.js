87a8439a3ec4ccd78b70e94f5e5fd323dab4889d
Eli Shkurkin <eshkurkin@gmail.com>
2014-02-08 15:41:07 -0800

    added FB login

--- js/map.js
+++ js/map.js
@@ -1,37 +1,61 @@
 $(function(){
   google.maps.event.addDomListener(window, 'load', initialize);
   getGraphData();
-});
+  $('.name-input').on('submit', function(e){
+    e.preventDefault();
+    processName(e);
+  });
 
+/////////////
+// FB LOGIN
+/////////////
+
+  window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '697839106933842',
+    status     : true, // check login status
+    cookie     : true, // enable cookies to allow the server to access the session
+    xfbml      : true  // parse XFBML
+  });
+  FB.Event.subscribe('auth.authResponseChange', function(response) {
+    if (response.status === 'connected') {
+      testAPI();
+    } else if (response.status === 'not_authorized') {
+      FB.login();
+    } else {
+      FB.login();
+    }
+  });
+  };
+
+  (function(d){
+   var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement('script'); js.id = id; js.async = true;
+   js.src = "http://connect.facebook.net/en_US/all.js";
+   ref.parentNode.insertBefore(js, ref);
+  }(document));
+
+  function testAPI() {
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+      console.log('Good to see you, ' + response.name + '.');
+    });
+  }
+});
 
 /////////////////
 // GOOGLE MAPS
 ////////////////
 
 function initialize() {
-
   var mapOptions = {
     center: new google.maps.LatLng(0, 0),
     zoom: 2
   };
-
   map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-
-  $('.name-input').on('submit', function(e){
-    e.preventDefault();
-    processName(e);
-  });
-
 }
 
-function processName(e){
-  var latLng = new google.maps.LatLng(-25.363882,131.044922);
-  var popup = '<div id="content">' +
-  '<p>This is only a test</p>'
-  '</div>';
-
-  addMarker(latLng, popup);
-}
 
 function addMarker(latLng, popup) {
   var marker = new google.maps.Marker({
@@ -52,12 +76,18 @@ function addInfoWindow(marker, popup) {
   });
 }
 
-
 ////////////////
 // FACEBOOK API
 ////////////////
 
-function getGraphData() {
+
+function processName(e){
+  var name = $(e.target).find("input").val().toLowerCase();
+  var encodedName = encodeURI(name);
+  getByName(encodedName);
+}
+
+function getGraphData(user_id) {
   $.ajax({
     url: "http://graph.facebook.com/1036590399?fields=picture,name"
   }).done(function(response){
@@ -68,17 +98,16 @@ function getGraphData() {
 }
 
 
-///  working on this
-// function getByName(name) {
-//   $.ajax({
-//     url: "http://graph.facebook.com/search?q=frank%20turner&type=user"
-
-//   })
-// }
+function getByName(name) {
+  $.ajax({
+    url: "http://graph.facebook.com/search?q=joe&type=user&access_token=CAACEdEose0cBAPJr8yoHZAtqq5Te4HvlBazLBBSv3rjplyZCGvPtNGkR9LovHl2aslr47yVZCRmzyYZAHfyKC2jpil468NxfoezZChEZAZBicCNBwgnqFVarTHqYFZCfsxe3obpQekjNHIUYuTgI6S446WiIBehZCWZB5ZCZARZBQFGPvq9gLM82ZCEhrxwQdbxBrWZCxkZD"
+  }).done(function(response){
+    debugger
+  })
+}
 
 
 function processGraphData(response) {
-  debugger
   var latLng = new google.maps.LatLng(0,0);
   var popup = '<img src="' + response.picture.data.url + '">' +
   '<p>' +  response.name + '</p>';
@@ -86,3 +115,5 @@ function processGraphData(response) {
 }
 
 
+// https://graph.facebook.com/oauth/access_token?client_id=697839106933842&client_secret=ee0d7aec350977e215b54ce6e9856ad6&grant_type=client_cred
+// access_token=697839106933842|PHYWDQwCwbxQtyKuz2sVuDVkbQQ
